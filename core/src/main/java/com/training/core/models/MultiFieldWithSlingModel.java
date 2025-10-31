32533af4220cfdbfc9662d4cdf5b17ccdcd13c45
aemdeveloper12 <rupanjaliakula@gmail.com>
2025-10-31 07:49:20 +0530

    Sling Models PostConstruct

--- core/src/main/java/com/training/core/models/MultiFieldWithSlingModel.java
+++ core/src/main/java/com/training/core/models/MultiFieldWithSlingModel.java
@@ -1,11 +1,17 @@
 package com.training.core.models;
 
-import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Exporter;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.ChildResource;
+import org.apache.sling.models.annotations.injectorspecific.RequestAttribute;
+import org.apache.sling.models.annotations.injectorspecific.Self;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 /*
@@ -13,13 +19,43 @@ import java.util.List;
 //https://sling.apache.org/documentation/bundles/models.html
 */
 
-@Model(adaptables= Resource.class,defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+@Model(adaptables= SlingHttpServletRequest.class,defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL,resourceType = "sunnycloud/components/multifieldwithslingmodel")
+@Exporter(name="jackson",extensions ="json")
 public class MultiFieldWithSlingModel {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Self
+    SlingHttpServletRequest slingHttpServletRequest;
+
     @ValueMapValue
     private String[] states;
 
     @ChildResource
     List<CountrywithCurrencies> multicountryandcurrency;
+
+    @RequestAttribute
+    private String value1;
+
+    @RequestAttribute
+    private String value2;
+
+    private String newData;
+
+    @PostConstruct
+    public void init(){
+        logger.info("I am executing after the ValueMapValue and Request Attributes");
+        String path = slingHttpServletRequest.getResource().getPath();
+        logger.info("Current Resource Path: {}", path);
+        if(value1!=null && value2!=null){
+            newData= value1.concat(value2);
+        }else{
+            logger.warn("Request attributes value1 or value2 are null. Setting newData to empty");
+            newData="";
+        }
+        logger.info("NewData from PostConstruct init Method::::::::::::::::"+newData);
+        logger.info("Completed the init Method");
+
+    }
 
     public String[] getStates() {
         return states;
@@ -27,5 +63,9 @@ public class MultiFieldWithSlingModel {
 
     public List<CountrywithCurrencies> getMulticountryandcurrency() {
         return multicountryandcurrency;
+    }
+
+    public String getNewData() {
+        return newData;
     }
 }
