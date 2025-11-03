7fbb3cb413e7dc811cc3a0b3339b3afea7722648
aemdeveloper12 <rupanjaliakula@gmail.com>
2025-11-03 07:47:09 +0530

    Component with adapters

---
+++ core/src/main/java/com/training/core/models/SampleModelWithAdapters.java
@@ -0,0 +1,70 @@
+package com.training.core.models;
+
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.PageManager;
+import lombok.Getter;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ValueMap;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.Self;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+import javax.annotation.PostConstruct;
+
+@Model(adaptables = SlingHttpServletRequest.class,
+        adapters = SampleInterface.class,
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+@Getter
+public class SampleModelWithAdapters implements SampleInterface {
+
+    @ValueMapValue
+    private String sampleText;
+
+    @ValueMapValue
+    private String sampleTextArea;
+
+    @ValueMapValue
+    private String path;
+
+    @Self
+    SlingHttpServletRequest slingHttpServletRequest;
+
+    @SlingObject
+    private Resource currentResource;
+
+    @SlingObject
+    private ResourceResolver resourceResolver;
+
+    private String pageTitleViaSling;
+    private  String pageTitleViaAEM;
+
+    @PostConstruct
+    public void init(){
+        PageManager pageManager= resourceResolver.adaptTo(PageManager.class);
+        if(pageManager!=null){
+            Page currentPage = pageManager.getContainingPage(currentResource);
+
+            if(currentPage!=null){
+                //By Using Sling ValueMap(reads directly from jcr:content)
+                Resource jcrContent = currentPage.getContentResource();
+                if(jcrContent!=null){
+                    ValueMap vm = jcrContent.adaptTo(ValueMap.class);
+                    pageTitleViaSling = vm.get("jcr:title", String.class);
+                }
+
+                // By Using Page API
+                pageTitleViaAEM= currentPage.getTitle();
+
+            }
+        }
+
+
+    }
+
+
+
+}
